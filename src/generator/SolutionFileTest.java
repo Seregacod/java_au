8ae52673bc16dd172d33b513e5520ce2f3b15899
Seregacod <71344196+Seregacod@users.noreply.github.com>
2022-01-19 21:41:09 +0300

    Create SolutionFileTest.java

---
+++ src/generator/SolutionFileTest.java
@@ -0,0 +1,63 @@
+package generator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionFileTest {
+    public static final List<String> MD_EMPTY_FILE_CONTENT = Collections.singletonList("");
+    public static final String MD_TITLE = "Reverse Linked List";
+    public static final String MD_URL = "https://leetcode.com/problems/reverse-linked-list/";
+    public static final String MD_SOLUTION = """
+            ```java
+            while head != None:
+                prev = head
+                head = n_node
+            return prev
+            ```
+            """;
+
+    public static final String MD_RESULT_STRING = "# OUTPUT\n\n" +
+            "+ ["+ MD_TITLE + "](#reverse-linked-list)\n" +
+            "<!---->\n\n" + "## " + MD_TITLE + "\n\n" + MD_URL + "\n\n" + MD_SOLUTION;
+
+    public List<String> MD_NON_EMPTY_FILE = Arrays.asList("# TESTOUTPUT",
+            "\n",
+            "+ [Reverse Linked List](#reverse-linked-list)",
+            "<!---->",
+            "\n",
+            "## Reverse Linked List",
+            "\n",
+            "https://leetcode.com/problems/reverse-linked-list/",
+            "\n",
+            "```java",
+            "while head != None:",
+            "    prev = head",
+            "    head = n_node",
+            "return prev",
+            "```");
+
+    @Test
+    void testNotEmptyMarkdownFile() {
+        SolutionFile res = SolutionFile.parseFile(MD_NON_EMPTY_FILE, Application.FileType.MARKDOWN, "output");
+        List<ItemEntity> lst = List.of(new MarkdownEntity(MD_TITLE, MD_URL, MD_SOLUTION));
+        SolutionFile expect = new SolutionFile(lst, Application.FileType.MARKDOWN, "output");
+        assertEquals(expect, res);
+    }
+
+    @Test
+    void testEmptyMarkdownFile() {
+        SolutionFile res = SolutionFile.parseFile(MD_EMPTY_FILE_CONTENT, Application.FileType.MARKDOWN, "output");
+        List<ItemEntity> lst = List.of();
+        SolutionFile expect = new SolutionFile(lst, Application.FileType.MARKDOWN, "output");
+        assertEquals(expect, res);
+    }
+
+    @Test
+    void testToMarkdownString() {
+        SolutionFile result = SolutionFile.parseFile(MD_NON_EMPTY_FILE, Application.FileType.MARKDOWN, "output");
+        assertEquals(MD_RESULT_STRING, result.toString());
+    }
+}
