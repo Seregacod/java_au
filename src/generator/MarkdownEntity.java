cd9fd8676478a3a08941d55b0e4407e3352c6cdb
Seregacod <71344196+Seregacod@users.noreply.github.com>
2022-01-19 21:40:06 +0300

    Create MarkdownEntity.java

---
+++ src/generator/MarkdownEntity.java
@@ -0,0 +1,54 @@
+package generator;
+
+import java.util.List;
+import java.util.Objects;
+
+public class MarkdownEntity implements ItemEntity {
+
+    private final String taskTitle;
+    private final String taskUrl;
+    private final String taskSolution;
+    private final String testCases;
+
+    public MarkdownEntity(String title, String url, String solution) {
+        this.taskTitle = title;
+        this.taskUrl = url;
+        this.taskSolution = solution;
+
+        this.testCases = "<details>\n" +
+                "    <summary> Test Cases </summary>\n" +
+                "\n" +
+                "    ``` java\n" +
+                "    \n" +
+                "    ``` \n" +
+                "</details>";
+    }
+
+    public static MarkdownEntity parseEntity(List<String> s) {
+        if (s.size() < 5) {
+            return new MarkdownEntity("", "", "");
+        }
+        String solution = "```java" + "\n" + String.join("\n", s.subList(4, s.size())) + "\n```\n";
+        return new MarkdownEntity(s.get(0), s.get(2), solution);
+    }
+
+    @Override
+    public String getTitle() {
+        return "+ [" + this.taskTitle + "]" + "(#" + this.taskUrl.split("/")[4] + ")";
+    }
+
+    @Override
+    public String getFormatted() {
+
+        return "## " + this.taskTitle + "\n\n" + this.taskUrl + "\n\n" + this.testCases +"\n\n" + this.taskSolution;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MarkdownEntity that = (MarkdownEntity) o;
+        return Objects.equals(taskTitle, that.taskTitle) && Objects.equals(taskUrl, that.taskUrl) && Objects.equals(taskSolution, that.taskSolution);
+    }
+
+}
